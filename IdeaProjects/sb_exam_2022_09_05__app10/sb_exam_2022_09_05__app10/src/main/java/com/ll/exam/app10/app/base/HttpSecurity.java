cf25024cdbd65f315b6a0470d27948acfa3f6b68
JI-NAMEUN <skadms1720@github.com>
2022-09-22 00:08:17 +0900

    csrf 체크 끄기

--- IdeaProjects/sb_exam_2022_09_05__app10/sb_exam_2022_09_05__app10/src/main/java/com/ll/exam/app10/app/base/HttpSecurity.java
+++ IdeaProjects/sb_exam_2022_09_05__app10/sb_exam_2022_09_05__app10/src/main/java/com/ll/exam/app10/app/base/HttpSecurity.java
@@ -1,9 +1,14 @@
 package main.java.com.ll.exam.app10.app.base;
 
+import com.fasterxml.jackson.core.JsonFactory;
+
 public class HttpSecurity {
     public Object authorizeRequests() {
     }
 
     public SecurityFilterChain build() {
+    }
+
+    public JsonFactory csrf() {
     }
 }
--- IdeaProjects/sb_exam_2022_09_05__app10/sb_exam_2022_09_05__app10/src/main/java/com/ll/exam/app10/app/base/SecurityConfig.java
+++ IdeaProjects/sb_exam_2022_09_05__app10/sb_exam_2022_09_05__app10/src/main/java/com/ll/exam/app10/app/base/SecurityConfig.java
@@ -1,21 +1,19 @@
 package com.ll.exam.app10.app.base;
-
-import main.java.com.ll.exam.app10.app.base.Configuration;
-import main.java.com.ll.exam.app10.app.base.EnableWebSecurity;
-import main.java.com.ll.exam.app10.app.base.HttpSecurity;
-import main.java.com.ll.exam.app10.app.base.SecurityFilterChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
         return http.build();
     }
 }
