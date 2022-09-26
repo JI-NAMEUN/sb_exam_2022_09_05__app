3166ff4d5fa3f40bc155a4fae1b165907f837427
JI-NAMEUN <skadms1720@github.com>
2022-09-26 09:45:41 +0900

    잔디 내놔

--- IdeaProjects/sb_exam_2022_09_05__app10/sb_exam_2022_09_05__app10/src/main/java/com/ll/exam/app10/app/base/SecurityConfig.java
+++ IdeaProjects/sb_exam_2022_09_05__app10/sb_exam_2022_09_05__app10/src/main/java/com/ll/exam/app10/app/base/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.ll.exam.app10.app.base;
 import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -15,5 +21,9 @@ public class SecurityConfig {
                 .antMatchers("/**")
                 .permitAll();
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- IdeaProjects/sb_exam_2022_09_05__app10/sb_exam_2022_09_05__app10/src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
+++ IdeaProjects/sb_exam_2022_09_05__app10/sb_exam_2022_09_05__app10/src/main/java/com/ll/exam/app10/app/member/controller/MemberController.java
@@ -1,21 +1,33 @@
 package com.ll.exam.app10.app.member.controller;
-
 import com.ll.exam.app10.app.member.entity.Member;
 import com.ll.exam.app10.app.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.ui.Model;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/join")
     public String showJoin() {
@@ -23,32 +35,33 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String join(String username, String password, String email, MultipartFile profileImg, HttpSession session) {
+    public String join(HttpServletRequest req, String username, String password, String email, MultipartFile profileImg) throws ServletException {
         Member oldMember = memberService.getMemberByUsername(username);
 
         if (oldMember != null) {
             return "redirect:/?errorMsg=Already done.";
         }
 
-        Member member = memberService.join(username, "{noop}" + password, email, profileImg);
+        String passwordClearText = password;
 
-        session.setAttribute("loginedMemberId", member.getId());
+        password = passwordEncoder.encode(password);
+
+        Member member = memberService.join(username, password, email, profileImg);
+
+        req.login(member.getUsername(), passwordClearText);
 
         return "redirect:/member/profile";
     }
 
     @GetMapping("/profile")
-    public String showProfile(HttpSession session, Model model) {
-    public String showProfile(HttpSession session) {
-        Long loginedMemberId = (Long) session.getAttribute("loginedMemberId");
-        boolean isLogined = loginedMemberId != null;
-
-        if (isLogined == false) {
+    public String showProfile(Principal principal, Model model) {
+        if (principal == null) {
             return "redirect:/?errorMsg=Need to login!";
         }
-            Member loginedMember = memberService.getMemberById(loginedMemberId);
 
-            model.addAttribute("loginedMember", loginedMember);
+        Member loginedMember = memberService.getMemberByUsername(principal.getName());
+
+        model.addAttribute("loginedMember", loginedMember);
 
         return "member/profile";
     }
